ff31b90c6848b2d5d1442d7560e7091267ce7c48
Shriya <sanand20@stuy.edu>
2020-11-01 17:48:26 -0500

    perimeter done

--- Triangle.java
+++ Triangle.java
@@ -3,16 +3,17 @@ public class Triangle {
   Point v2;
   Point v3;
   public Triangle(Point a, Point b, Point c) {
-  Point v1 = a;
-  Point v2 = b;
-  Point v3 = c;
+  v1 = a;
+  v2 = b;
+  v3 = c;
   }
   public Triangle(double x1, double y1,double x2, double y2,double x3, double y3){
-    Point v1 = Point (x1, y1);
-    Point v2 = Point (x2, y2);
-    Point v3 = Point (x3, y3);
+    v1 = new Point (x1, y1);
+    v2 = new Point (x2, y2);
+    v3 = new Point (x3, y3);
   }
-//  public double getPerimeter() {
-  //  return v1.distanceTo(v2) + v2.distanceTo(v3) + v1.distanceTo(v3);
-  //}
+  public double getPerimeter() {
+    return v1.distanceTo(v2) + v2.distanceTo(v3) + v1.distanceTo(v3);
+    }
+  
 }
