4a0583d10717f0e889070dba42c314008021676b
Shriya <sanand20@stuy.edu>
2020-11-01 18:01:47 -0500

    area/classify done

--- Triangle.java
+++ Triangle.java
@@ -15,5 +15,21 @@ public class Triangle {
   public double getPerimeter() {
     return v1.distanceTo(v2) + v2.distanceTo(v3) + v1.distanceTo(v3);
     }
+  public double getArea() {
+      double s = .5 * getPerimeter();
+      return Math.sqrt (s * (s-v1.distanceTo(v2))* (s-v1.distanceTo(v3))* (s-v2.distanceTo(v3)));
+    }
+  public String classify() {
+    double a = Math.round(v1.distanceTo(v2)*10000)/10000;
+    double b = Math.round(v2.distanceTo(v3)*10000)/10000;
+    double c = Math.round(v1.distanceTo(v3)*10000)/10000;
+    if (a==b && b==c){
+      return "equilateral";
+    }
+    else if (a==b || a==c || b==c ){
+      return "isosceles";
+    }
+    else {return "scalene";}
+  }
   
 }
